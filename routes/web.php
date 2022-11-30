2f1fae5d0f9f2295e4d166e7f31d8f95ac4e65a2
hilmyha <hilmyahmadhaidar@gmail.com>
2022-11-30 19:05:01 +0700

    Model, Collection & Controller | Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -31,19 +32,7 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    
-    return view('posts', [
-        'title' => 'Posts',
-        'posts' => Post::all(),
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // single posts
-Route::get('posts/{slug}', function ($slug) {
-
-    return view('post', [
-        'title' => 'Single Post',
-        'post' => Post::find($slug),
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
