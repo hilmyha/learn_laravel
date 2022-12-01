e99057406dc9b371716c9c0bdc1c3855ce64ab4a
hilmyha <hilmyahmadhaidar@gmail.com>
2022-12-01 11:30:16 +0700

    Post Model | Make Post and Blade Escape Character

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,40 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-// class Post extends Model
-// {
-//     use HasFactory;
-// }
-
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'author' => 'administrator',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ex cumque perferendis ab, vero doloribus rerum impedit nulla et, deleniti eaque inventore! A accusantium fugiat eaque, nam neque perspiciatis quidem, veritatis quibusdam doloribus adipisci facilis, ipsam cupiditate reprehenderit aut fugit? Officia consequuntur quod repudiandae vel error nisi animi ullam quibusdam numquam quo est eius voluptatem, tempore fugit. Pariatur fugiat ea provident nobis iste illum sint repudiandae minus ullam placeat, temporibus omnis eaque laboriosam ab esse cupiditate excepturi deleniti repellat sapiente fuga laborum fugit earum molestiae. Ut, deleniti vel esse ipsum, ex, laboriosam ad nam dolorum animi facere amet quae earum.',
-        ],
-        [
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'author' => 'administrator',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ex cumque perferendis ab, vero doloribus rerum impedit nulla et, deleniti eaque inventore! A accusantium fugiat eaque, nam neque perspiciatis quidem, veritatis quibusdam doloribus adipisci facilis, ipsam cupiditate reprehenderit aut fugit? Officia consequuntur quod repudiandae vel error nisi animi ullam quibusdam numquam quo est eius voluptatem, tempore fugit.',
-        ]
-    ];
-    
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    use HasFactory;
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,9 +2,9 @@
 
 @section('container')
   <div class="border grid gap-2 mb-8">
-    <h2>{{ $post['title'] }}</h2>
-    <h6 class="text-h6">By : {{ $post['author'] }}</h6>
-    <p>{{ $post['body'] }}</p>
+    <h2>{{ $post->title }}</h2>
+    <h6 class="text-h6">By : {{ $post->author }}</h6>
+    {!! $post->body !!} 
   </div>
 
   <a href="/blog">Back to Post</a>
