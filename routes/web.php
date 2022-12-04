28066dd090bf3dafb33b72c89e572492c882090f
hilmyha <hilmyahmadhaidar@gmail.com>
2022-12-04 16:06:11 +0700

    N+1 Problem | Lazy Eager Loading

--- routes/web.php
+++ routes/web.php
@@ -48,13 +48,13 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
-        'posts' => $category->posts,
+        'posts' => $category->posts->load('category', 'author'),
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => "Post by Author : $author->name",
-        'posts' => $author->post,
+        'posts' => $author->post->load('category', 'author'),
     ]);
 });
